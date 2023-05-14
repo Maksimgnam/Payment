a288e415fa3cd94d14f9f6000baa197e46473b21
UserName <m492615892gmail.com>
2023-05-14 17:53:23 +0300

    m

--- src/Popup/Popup.jsx
+++ src/Popup/Popup.jsx
@@ -6,21 +6,29 @@ const Popup = () => {
 
     const [close, setClose] = useState(true)
     const [sum, setSum] = useState(0)
+    const [inputVal, setInputVal] = useState('')
 
     const Close = () => {
         setClose(false)
+
     }
     const addHundret = () => {
         setSum(sum + 100)
+        setInputVal(inputVal + 100)
     }
     const addFive = () => {
         setSum(sum + 500)
+        setInputVal(inputVal + 500)
     }
     const addThousand = () => {
         setSum(sum + 1000)
+        setInputVal(inputVal + 1000)
     }
     const ThankYou = () => {
         alert('Thank you for ' + sum)
+    }
+    const Inp = (e) => {
+        setInputVal(e.target.value)
     }
     return (
         <>
@@ -33,7 +41,7 @@ const Popup = () => {
                             <h4>Накопичено {sum}</h4>
                         </div>
                         <div className="Earn">
-                            <p>{sum}</p>
+                            <input type="text" className='sum' value={inputVal} onChange={Inp} />
                         </div>
                         <div className="Buttons">
                             <button onClick={addHundret}>100</button>
